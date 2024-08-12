1b44177d8f647f65a7c619f6b963d13cdf2e2340
Dmitrii <aleksdima@mail.ru>
2024-08-12 14:09:03 +0300

    Homework 2 completed

---
+++ src/main/java/Homework2/FactoryMethod/LogEntry.java
@@ -0,0 +1,13 @@
+package Homework2.FactoryMethod;
+
+public class LogEntry {
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public LogEntry(String text) {
+        this.text = text;
+    }
+}
---
+++ src/main/java/Homework2/FactoryMethod/LogReader.java
@@ -0,0 +1,27 @@
+package Homework2.FactoryMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class LogReader {
+private int currentPosition=0;
+
+    public int getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public void setCurrentPosition(int currentPosition) {
+        this.currentPosition = currentPosition;
+    }
+    public Iterable<LogEntry> readLogEntry(){
+        List<LogEntry> logList=new ArrayList<>();
+        for (String str:readEntries(currentPosition)){
+            logList.add(parseLogEntry(str));
+        }
+        return logList;
+    }
+    public abstract Object getDataSource();
+    public abstract void setDataSource(Object data);
+    protected abstract Iterable<String> readEntries(Integer position);
+    protected abstract LogEntry parseLogEntry(String stringEntry);
+}
